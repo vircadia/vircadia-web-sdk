b1d374c3d3a222dcda1fe86666022c6be876f16c
David Rowe <david@ctrlaltstudio.com>
2021-05-10 12:48:17 +1200

    SDK stub

---
+++ src/Vircadia.js
@@ -0,0 +1,25 @@
+//
+//  Vircadia.js
+//
+//  Vircadia Web SDK.
+//
+//  Created by David Rowe on 9 May 2021.
+//  Copyright 2021 Vircadia contributors.
+//
+//  Distributed under the Apache License", Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+const Vircadia = (function () {
+
+    function helloWorld() {
+        console.log("Hello world!");
+    }
+
+    return {
+        helloWorld
+    };
+
+}());
+
+export default Vircadia;
