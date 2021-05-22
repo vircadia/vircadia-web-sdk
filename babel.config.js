81ceabb0f1833d959847e1d00d60a33256bd1043
David Rowe <david@ctrlaltstudio.com>
2021-05-22 20:45:55 +1200

    Fix up using Babel with Jest

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,15 @@
-module.exports = {
-    presets: [
-        [
-            "@babel/preset-env", {
-                "shippedProposals": true
-            }
-        ]
-    ]
+module.exports = (api) => {
+
+    if (api.env("test")) {
+        // Jest test environment.
+        return {
+            presets: [["@babel/preset-env", { targets: { node: "current" } }]]
+        };
+    }
+
+    // Watch, build, etc.
+    return {
+        presets: [["@babel/preset-env", { "shippedProposals": true }]]
+    };
+
 };
