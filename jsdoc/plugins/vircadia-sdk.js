550501627d5f9232265b04e1a6920b018bfda4ac
David Rowe <david@ctrlaltstudio.com>
2021-06-05 22:07:34 +1200

    Fix processing developer and SDK JSDoc

--- jsdoc/plugins/vircadia-sdk.js
+++ jsdoc/plugins/vircadia-sdk.js
@@ -1,52 +1,8 @@
-function endsWith(path, exts) {
-    var result = false;
-    exts.forEach(function (ext) {
-        if (path.endsWith(ext)) {
-            result = true;
-        }
-    });
-    return result;
-}
-
 exports.handlers = {
 
-    // This event is triggered before parsing has started.
-    // Scan the JavaScript files for JSDoc comments and reformat to enable.
+    // This event is triggered for each file.
     beforeParse: function (e) {
-        var pathTools = require('path');
-        var rootFolder = pathTools.dirname(e.filename);
-        console.log("Scanning the Vircadia source for JSDoc comments...");
-
-        // Directories to scan for JSDoc comments.
-        var dirList = [
-            '.'
-        ];
-
-        // Files with these extensions will be searched for JSDoc comments.
-        var exts = ['.js', '.mjs'];
-
-        var fs = require('fs');
-        dirList.forEach(function (dir) {
-            var joinedDir = pathTools.join(rootFolder, dir);
-            var files = fs.readdirSync(joinedDir);
-            files.forEach(function (file) {
-                var path = pathTools.join(joinedDir, file);
-                if (fs.lstatSync(path).isFile() && endsWith(path, exts)) {
-                    // load entire file into a string
-                    var data = fs.readFileSync(path, "utf8");
-
-                    // this regex searches for blocks starting with /*@sdkdoc and ending with */
-                    var reg = /(\/\*@sdkdoc(.|[\r\n])*?\*\/)/gm;
-                    var matches = data.match(reg);
-                    if (matches) {
-                        // add to source, but strip off c-comment asterisks
-                        e.source += matches.map(function (s) {
-                            return s.replace('/*@sdkdoc', '/**');
-                        }).join('\n');
-                    }
-                }
-            });
-        });
+        e.source = e.source.replace(/\/\*@sdkdoc/g, "/**");
     },
 
     processingComplete: function (e) {
