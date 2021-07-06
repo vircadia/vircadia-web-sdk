9845ef7f51ddff70e00ad652af96653dbbe79f8e
David Rowe <david@ctrlaltstudio.com>
2021-07-06 16:46:07 +1200

    Fix license typo

--- tests/libraries/networking/PacketReceiver.unit.test.js
+++ tests/libraries/networking/PacketReceiver.unit.test.js
@@ -4,7 +4,7 @@
 //  Created by David Rowe on 8 Jun 2021.
 //  Copyright 2021 Vircadia contributors.
 //
-//  Distributed under the Apache License", Version 2.0.
+//  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
 
