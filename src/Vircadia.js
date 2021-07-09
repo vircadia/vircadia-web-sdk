e07cf2313463636f6f57c36f08d44bdff829b830
Kalila L <somnilibertas@gmail.com>
2021-07-09 01:33:51 -0400

    Update index.js -> Vircadia.js.

--- src/Vircadia.js
+++ src/Vircadia.js
@@ -1,4 +1,4 @@
-//  index.js
+//  Vircadia.js
 //
 //  Created by Kalila L. on July 8th, 2021.
 //  Copyright 2021 Vircadia contributors.
@@ -6,7 +6,5 @@
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
-//  This is the default export and therefore all core classes under the
-//  Vircadia Web SDK should be imported and subsequently exported here.
 
-export { default } from "./src/DomainServer.js";
+export { default } from "./DomainServer.js";
