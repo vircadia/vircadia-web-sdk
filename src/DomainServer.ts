b2fd34313ed774cb256f62174def494b31639646
David Rowe <david@ctrlaltstudio.com>
2021-07-25 12:46:12 +1200

    Implement DomainServer API shell

--- src/DomainServer.ts
+++ src/DomainServer.ts
@@ -10,21 +10,173 @@
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
 
+
 /*@sdkdoc
- *  The <code>DomainServer</code> API provides the interface for connecting to and interacting with a domain server.
- *
- *  @namespace DomainServer
- *  @property {boolean} isConnected - <code>true</code> if connected to the domain, <code>false</code> if not connected.
- *      <em>Read-only.</em>
+ *  <table>
+ *      <thead>
+ *          <tr><th>Name</th><th>Value</th><th>Description</th></tr>
+ *      </thead>
+ *      <tbody>
+ *          <tr><td>DISCONNECTED</td><td>0</td><td>Disconnected from the domain.</td></tr>
+ *          <tr><td>CONNECTING</td><td>1</td><td>Connecting to the domain.</td></tr>
+ *          <tr><td>CONNECTED</td><td>2</td><td>Connected to the domain.</td></tr>
+ *          <tr><td>REFUSED</td><td>3</td><td>Connection to the domain refused; not connected to the domain.</td></tr>
+ *          <tr><td>ERROR</td><td>4</td><td>Error connecting to the domain; not connected to the domain.</td></tr>
+ *      </tbody>
+ *  </table>
+ *  @typedef {number} DomainServer.ConnectionState
  */
-const DomainServer = new class {
+enum ConnectionState {
+    DISCONNECTED = 0,
+    CONNECTING,
+    CONNECTED,
+    REFUSED,
+    ERROR
+}
 
-    #_isConnected = false;
+type OnStateChangedCallback = (state: ConnectionState, info: string) => void;
 
-    get isConnected() {
-        return this.#_isConnected;
+
+/*@sdkdoc
+ *  The <code>DomainServer</code> API provides the interface for connecting to a domain server.
+ *
+ *  @class DomainServer
+ *  @property {DomainServer.ConnectionState} DISCONNECTED - Disconnected from the domain.
+ *      <em>Static. Read-only.</em>
+ *  @property {DomainServer.ConnectionState} CONNECTING - Connecting to the domain.
+ *      <em>Static. Read-only.</em>
+ *  @property {DomainServer.ConnectionState} CONNECTED - Connected to the domain.
+ *      <em>Static. Read-only.</em>
+ *  @property {DomainServer.ConnectionState} REFUSED - Connection to the domain refused; not connected to the domain. See
+ *      <code>refusalInfo</code> for details.
+ *      <em>Static. Read-only.</em>
+ *  @property {DomainServer.ConnectionState} ERROR - Error connecting to the domain; not connected to the domain. See
+ *      <code>errorInfo</code> for details.
+ *      <em>Static. Read-only.</em>
+ *  @property {string} location - The current location that the domain server is pointed at. <code>""</code> if no location has
+ *      been set.
+ *      <em>Read-only.</em>
+ *  @property {DomainServer.ConnectionState} state - The current state of the connection to the domain server.
+ *      <em>Read-only.</em>
+ *  @property {string} refusalError - A description of the reason if <code>state == DomainServer.REFUSED</code>, otherwise
+ *      <code>""</code>.
+ *      <em>Read-only.</em>
+ *  @property {string} errorError - A description of the reason if <code>state == DomainServer.ERROR</code>, otherwise
+ *      <code>""</code>.
+ *      <em>Read-only.</em>
+ *  @property {DomainServer~onStateChangedCallback} onStateChanged - Sets a single function to be called when the state of the
+ *      domain server connection changes.
+ *      <em>Write-only.</em>
+ */
+class DomainServer {
+
+    /*@sdkdoc
+     *  Called when the state of the domain server connection changes.
+     *  @callback DomainServer~onStateChangedCallback
+     *  @param {DomainServer.ConnectionState} state - The state of the domain server connection.
+     *  @param {string} info - Refusal or error information if the state is <code>REFUSAL</code> or <code>ERROR</code>.
+     */
+
+    static get DISCONNECTED(): ConnectionState {
+        return ConnectionState.DISCONNECTED;
     }
 
-}();
+    static get CONNECTING(): ConnectionState {
+        return ConnectionState.CONNECTING;
+    }
+
+    static get CONNECTED(): ConnectionState {
+        return ConnectionState.CONNECTED;
+    }
+
+    static get REFUSED(): ConnectionState {
+        return ConnectionState.REFUSED;
+    }
+
+    static get ERROR(): ConnectionState {
+        return ConnectionState.ERROR;
+    }
+
+
+    #_location = "";
+    #_state: ConnectionState = DomainServer.DISCONNECTED;
+    #_refusalInfo = "";
+    #_errorInfo = "";
+    #_onStateChangedCallback: OnStateChangedCallback | null = null;
+
+
+    get location(): string {
+        return this.#_location;
+    }
+
+    get state(): ConnectionState {
+        return this.#_state;
+    }
+
+    get refusalInfo(): string {
+        return this.#_refusalInfo;
+    }
+
+    get errorInfo(): string {
+        return this.#_errorInfo;
+    }
+
+    set onStateChanged(callback: OnStateChangedCallback) {
+        if (typeof callback !== "function") {
+            this.#_onStateChangedCallback = null;
+        }
+        this.#_onStateChangedCallback = callback;
+    }
+
+
+    /*@sdkdoc
+     *  Initiates connection to a Domain Server.
+     *  <p>The following types of location are supported:</p>
+     *  <table>
+     *      <tbody>
+     *          <tr><td ><code>WEBRTC TODO</code></td><td>WEBRTC TODO</td></tr>
+     *      </tbody>
+     *  </table>
+     *  @param {string} location - The location of the Domain Server to connect to.
+     */
+    connect(location: string): void {
+
+        this.#_location = typeof location === "string" ? location.trim() : "";
+
+        if (this.#_location === "") {
+            this.#setState(DomainServer.ERROR, "No location specified.");
+            return;
+        }
+
+        this.#setState(DomainServer.CONNECTING);
+
+        // WEBRTC TODO
+
+    }
+
+    /*@sdkdoc
+     *  Disconnects from the domain server.
+     */
+    disconnect(): void {
+        this.#setState(DomainServer.DISCONNECTED);
+
+        // WEBRT TODO
+    }
+
+
+    #setState(state: ConnectionState, info = ""): void {
+        this.#_state = state;
+        this.#_refusalInfo = "";
+        this.#_errorInfo = "";
+        if (this.#_state === DomainServer.REFUSED) {
+            this.#_refusalInfo = info;
+        } else if (this.#_state === DomainServer.ERROR) {
+            this.#_errorInfo = info;
+        }
+        if (this.#_onStateChangedCallback) {
+            this.#_onStateChangedCallback(state, info);
+        }
+    }
+}
 
 export default DomainServer;
--- tests/DomainServer.unit.test.js
+++ tests/DomainServer.unit.test.js
@@ -13,16 +13,143 @@ import DomainServer from "../src/DomainServer";
 
 describe("DomainServer - unit tests", () => {
 
-    test("Is connected", () => {
-        expect(DomainServer.isConnected).toBe(false);
+    /* eslint-disable @typescript-eslint/no-magic-numbers */
+
+    test("Can get but not set location property", () => {
+        const domainServer = new DomainServer();
+        expect(domainServer.location).toBe("");
+        let caughtError = false;
         try {
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            DomainServer.isConnected = true;  // Shouldn't succeed.
+            domainServer.location = "location";  // Shouldn't succeed;
         } catch (e) {
             //
+            caughtError = true;
         }
-        expect(DomainServer.isConnected).toBe(false);
+        expect(caughtError).toBe(true);
+        expect(domainServer.location).toBe("");
     });
+
+    test("Can get but not set connection state values", () => {
+        expect(DomainServer.DISCONNECTED).toBe(0);
+        expect(DomainServer.ERROR).toBe(4);
+        expect(DomainServer.CONNECTING).toBe(1);
+        let caughtError = false;
+        try {
+            DomainServer.CONNECTING = 2;  // Shouldn't succeed;
+        } catch (e) {
+            //
+            caughtError = true;
+        }
+        expect(caughtError).toBe(true);
+        expect(DomainServer.CONNECTING).toBe(1);
+    });
+
+    test("Can get but not set current connection state", () => {
+        const domainServer = new DomainServer();
+        expect(domainServer.state).toBe(DomainServer.DISCONNECTED);
+        let caughtError = false;
+        try {
+            domainServer.state = DomainServer.CONNECTED;  // Shouldn't succeed;
+        } catch (e) {
+            //
+            caughtError = true;
+        }
+        expect(caughtError).toBe(true);
+        expect(domainServer.state).toBe(DomainServer.DISCONNECTED);
+    });
+
+    test("Can get but not set refusal and error info", () => {
+        const domainServer = new DomainServer();
+        expect(domainServer.refusalInfo).toBe("");
+        expect(domainServer.errorInfo).toBe("");
+        let caughtErrorCount = 0;
+        try {
+            domainServer.refusalInfo = "refused";  // Shouldn't succeed;
+        } catch (e) {
+            //
+            caughtErrorCount += 1;
+        }
+        try {
+            domainServer.errorInfo = "error";  // Shouldn't succeed;
+        } catch (e) {
+            //
+            caughtErrorCount += 1;
+        }
+        expect(caughtErrorCount).toBe(2);
+        expect(domainServer.refusalInfo).toBe("");
+        expect(domainServer.errorInfo).toBe("");
+    });
+
+    test("Error state if invalid location parameter specified", () => {
+        const domainServer = new DomainServer();
+        expect(domainServer.state).toBe(DomainServer.DISCONNECTED);
+        expect(domainServer.errorInfo).toBe("");
+        expect(domainServer.refusalInfo).toBe("");
+        domainServer.connect();  // undefined
+        expect(domainServer.state).toBe(DomainServer.ERROR);
+        expect(domainServer.errorInfo).not.toBe("");
+        expect(domainServer.refusalInfo).toBe("");
+    });
+
+    test("Error state if no location specified to connect to", () => {
+        const domainServer = new DomainServer();
+        expect(domainServer.state).toBe(DomainServer.DISCONNECTED);
+        expect(domainServer.errorInfo).toBe("");
+        expect(domainServer.refusalInfo).toBe("");
+        domainServer.connect("  ");
+        expect(domainServer.state).toBe(DomainServer.ERROR);
+        expect(domainServer.errorInfo).not.toBe("");
+        expect(domainServer.refusalInfo).toBe("");
+    });
+
+    test("Transitions to connecting state if location specified", () => {
+        const domainServer = new DomainServer();
+        const location = "hello";
+        expect(domainServer.state).toBe(DomainServer.DISCONNECTED);
+        expect(domainServer.errorInfo).toBe("");
+        domainServer.connect(location);
+        expect(domainServer.state).toBe(DomainServer.CONNECTING);
+        expect(domainServer.errorInfo).toBe("");
+        expect(domainServer.refusalInfo).toBe("");
+        expect(domainServer.location).toBe(location);
+    });
+
+    test("Can disconnect while connecting", () => {
+        const domainServer = new DomainServer();
+        const location = "hello";
+        domainServer.connect(location);
+        expect(domainServer.state).toBe(DomainServer.CONNECTING);
+        expect(domainServer.errorInfo).toBe("");
+        expect(domainServer.refusalInfo).toBe("");
+        expect(domainServer.location).toBe(location);
+        domainServer.disconnect();
+        expect(domainServer.state).toBe(DomainServer.DISCONNECTED);
+        expect(domainServer.errorInfo).toBe("");
+        expect(domainServer.refusalInfo).toBe("");
+        expect(domainServer.location).toBe(location);
+    });
+
+    test("State changed callback is called when transition to connecting state", (done) => {
+        const domainServer = new DomainServer();
+        domainServer.onStateChanged = (state, info) => {
+            expect(state).toBe(DomainServer.CONNECTING);
+            expect(info).toBe("");
+            done();
+        };
+        expect(domainServer.state).toBe(DomainServer.DISCONNECTED);
+        domainServer.connect("hello");
+    });
+
+    test("State changed callback is called when transition to error state", (done) => {
+        const domainServer = new DomainServer();
+        domainServer.onStateChanged = (state, info) => {
+            expect(state).toBe(DomainServer.ERROR);
+            expect(info).not.toBe("");
+            done();
+        };
+        expect(domainServer.state).toBe(DomainServer.DISCONNECTED);
+        domainServer.connect("");
+    });
+
 
 });
