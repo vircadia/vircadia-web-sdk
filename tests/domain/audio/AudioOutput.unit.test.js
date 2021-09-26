5b39e445e9c17e5100bfa0307249e65f54597a39
David Rowe <david@ctrlaltstudio.com>
2021-09-27 10:46:51 +1300

    Fix tests not working with the audio worklet loader

---
+++ tests/domain/audio/AudioOutput.unit.test.js
@@ -0,0 +1,24 @@
+//
+//  AudioOutput.unit.test.js
+//
+//  Created by David Rowe on 26 Sep 2021.
+//  Copyright 2021 Vircadia contributors.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+import AudioOutput from "../../../src/domain/audio/AudioOutput";
+import ContextManager from "../../../src/domain/shared/ContextManager";
+
+
+describe("AudioOutput - unit tests", () => {
+
+    test("The AudioOutput can be obtained from the ContextManager", () => {
+        const contextID = ContextManager.createContext();
+        ContextManager.set(contextID, AudioOutput);
+        const audioOutput = ContextManager.get(contextID, AudioOutput);
+        expect(audioOutput instanceof AudioOutput).toBe(true);
+    });
+
+});
