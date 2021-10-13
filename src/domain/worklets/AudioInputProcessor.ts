cf513d138a494748bf2a89512168d9e3bc4e85d5
namark <nshan.nnnn@gmail.com>
2021-10-13 23:16:35 +0400

    WIP Audio resampling
    it does not work because worklets do no run in window context, so will
    need to move all of it out of the worklet, leaving just some buffering.

--- src/domain/worklets/AudioInputProcessor.ts
+++ src/domain/worklets/AudioInputProcessor.ts
@@ -8,6 +8,13 @@
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
 
+import AudioConstants from "../audio/AudioConstants";
+import assert from "../shared/assert";
+
+const InputBuffer = Float32Array;
+const OutputBuffer = Int16Array;
+
+export type AudioInputProcessorOptions = { sourceSampleRate?: number };
 
 /*@devdoc
  *  The <code>AudioInputProcessor</code> class implements a Web Audio
@@ -32,23 +39,77 @@ class AudioInputProcessor extends AudioWorkletProcessor {
     readonly SDK_STEREO_BUFFER_SAMPLES = 480;
     readonly SDK_STEREO_BUFFER_BYTES = 960;
 
+
     _channelCount;
-    _samplesSize;
-    _bufferSize;
-    _buffer: Int16Array;
-    _bufferView: DataView;
-    _bufferIndex = 0;  // The next write position.
+    _outputSampleSize: number;
+    _accumulator: AudioBuffer;
+    _accumulatorIndex = 0;
+    _sourceSampleRate: number;
+    _bufferQeue: Promise<void> = Promise.resolve();
 
     constructor(options?: AudioWorkletNodeOptions) {
         super(options);  // eslint-disable-line
 
         this._channelCount = options?.channelCount ? options.channelCount : 1;  // Default to mono.
-        this._samplesSize = this._channelCount === 1 ? this.SDK_MONO_BUFFER_SAMPLES : this.SDK_STEREO_BUFFER_SAMPLES;
-        this._buffer = new Int16Array(this._samplesSize);
-        this._bufferView = new DataView(this._buffer.buffer);
-        this._bufferSize = this._channelCount === 1 ? this.SDK_MONO_BUFFER_BYTES : this.SDK_STEREO_BUFFER_BYTES;
+        this._outputSampleSize = this._channelCount === 1 ? this.SDK_MONO_BUFFER_SAMPLES : this.SDK_STEREO_BUFFER_SAMPLES;
+        const outputBufferSize = this._channelCount === 1 ? this.SDK_MONO_BUFFER_BYTES : this.SDK_STEREO_BUFFER_BYTES;
+
+        let accumulatorSize = outputBufferSize / this._channelCount / InputBuffer.BYTES_PER_ELEMENT;
+
+        const preprocessorOptions = options?.processorOptions as AudioInputProcessorOptions ?? {};
+        this._sourceSampleRate = preprocessorOptions?.sourceSampleRate || AudioConstants.SAMPLE_RATE;
+        if (this._sourceSampleRate !== AudioConstants.SAMPLE_RATE) {
+            const resampleRatio = AudioConstants.SAMPLE_RATE / this._sourceSampleRate;
+            accumulatorSize /= resampleRatio;
+        }
+
+        this._accumulator = this._createAccumulator(accumulatorSize);
 
         this.port.onmessage = this.onMessage;
+    }
+
+    _createAccumulator(size: number) {
+        return new window.AudioBuffer({
+            numberOfChannels: this._channelCount,
+            sampleRate: this._sourceSampleRate || AudioConstants.SAMPLE_RATE,
+            length: size
+        });
+    }
+
+    _resetAccumulator() {
+        this._accumulator = this._createAccumulator(this._accumulator.length);
+        this._accumulatorIndex = 0;
+    }
+
+    _resample(buffer: AudioBuffer): Promise<AudioBuffer> {
+        const context = new OfflineAudioContext({
+            numberOfChannels: buffer.numberOfChannels,
+            sampleRate: AudioConstants.SAMPLE_RATE,
+            length: this._outputSampleSize
+        });
+        const source = context.createBufferSource();
+        source.buffer = buffer;
+        source.connect(context.destination);
+        source.start();
+        return context.startRendering();
+    }
+
+    _send(buffer: AudioBuffer) {
+        const output = new OutputBuffer(this._outputSampleSize);
+        const view = new DataView(output);
+
+        assert(output.length === buffer.length * buffer.numberOfChannels);
+
+        const FLOAT_TO_INT = 32767;
+        const LITTLE_ENDIAN = true;
+
+        for (let i = 0; i < buffer.length; ++i) {
+            for (let j = 0; j < buffer.numberOfChannels; ++j) {
+                view.setInt16(i + j * 2, buffer.getChannelData(j)[i] as number * FLOAT_TO_INT, LITTLE_ENDIAN);
+            }
+        }
+
+        this.port.postMessage(output.buffer, [output.buffer]);
     }
 
     /*@devdoc
@@ -60,8 +121,7 @@ class AudioInputProcessor extends AudioWorkletProcessor {
      */
     onMessage = (message: MessageEvent) => {
         if (message.data === "clear") {
-            this._buffer = new Int16Array(this._samplesSize);
-            this._bufferView = new DataView(this._buffer.buffer);
+            this._resetAccumulator();
         }
     };
 
@@ -79,52 +139,37 @@ class AudioInputProcessor extends AudioWorkletProcessor {
             return true;
         }
 
-        const FLOAT_TO_INT = 32767;
-        const LITTLE_ENDIAN = true;
+        const input = inputList[0];
 
-        let index = this._bufferIndex;
+        let inputSize = inputList[0].length;
+        while (inputSize) {
 
-        const inputSamplesCount = inputList[0][0].length;
-        const bufferSize = this._bufferSize;
+            const remaining = this._accumulator.length - this._accumulatorIndex;
+            const accumulated = Math.min(remaining, inputSize);
+            const inputIndex = inputList[0].length - inputSize;
 
-        if (this._channelCount === 2) {
-            // Stereo.
-            const leftInput = inputList[0][0];
-            const rightInput = inputList[0][1] as Float32Array;
-            let bufferView = this._bufferView;
-            for (let i = 0; i < inputSamplesCount; i++) {
-                bufferView.setInt16(index, leftInput[i] as number * FLOAT_TO_INT, LITTLE_ENDIAN);
-                bufferView.setInt16(index + 2, rightInput[i] as number * FLOAT_TO_INT, LITTLE_ENDIAN);
-                index += 4;  // eslint-disable-line @typescript-eslint/no-magic-numbers
-                if (index === this._bufferSize) {
-                    // The output buffer is full; send it off.
-                    this.port.postMessage(this._buffer.buffer, [this._buffer.buffer]);
-                    this._buffer = new Int16Array(this._samplesSize);
-                    this._bufferView = new DataView(this._buffer.buffer);
-                    bufferView = this._bufferView;
-                    index = 0;
-                }
-            }
-        } else {
-            // Mono.
-            const monoInput = inputList[0][0];
-            let bufferView = this._bufferView;
-            for (let i = 0; i < inputSamplesCount; i++) {
-                bufferView.setInt16(index, monoInput[i] as number * FLOAT_TO_INT, LITTLE_ENDIAN);
-                index += 2;
-                if (index === bufferSize) {
-                    // The output buffer is full; send it off.
-                    this.port.postMessage(this._buffer.buffer, [this._buffer.buffer]);
-                    this._buffer = new Int16Array(this._samplesSize);
-                    this._bufferView = new DataView(this._buffer.buffer);
-                    bufferView = this._bufferView;
-                    index = 0;
-                }
+            for (let i = 0; i < this._accumulator.numberOfChannels; ++i) {
+                const channel = input[i] as Float32Array;
+                const inputView = new InputBuffer(channel.buffer, inputIndex * InputBuffer.BYTES_PER_ELEMENT);
+                this._accumulator.copyToChannel(inputView, i, this._accumulatorIndex);
             }
 
+            this._accumulatorIndex += accumulated;
+            inputSize -= accumulated;
+
+            if (this._accumulatorIndex === this._accumulator.length) {
+
+                if (this._sourceSampleRate !== AudioConstants.SAMPLE_RATE) {
+                    this._bufferQeue = this._bufferQeue
+                        .then(() => this._resample(this._accumulator))
+                        .then((resampled: AudioBuffer) => this._send(resampled));
+                } else {
+                    this._send(this._accumulator);
+                }
+
+                this._resetAccumulator();
+            }
         }
-
-        this._bufferIndex = index;
 
         return true;
     }
