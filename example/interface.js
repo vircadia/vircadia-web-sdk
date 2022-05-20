674d50f949884d2cab58f14a691a7d49239b4524
David Rowe <david@ctrlaltstudio.com>
2022-05-21 10:53:22 +1200

    Typos

--- example/interface.js
+++ example/interface.js
@@ -3,7 +3,7 @@
 //
 //  Created by David Rowe on 1 Aug 2021.
 //  Copyright 2021 Vircadia contributors.
-//  Copyright 2021 DifiSomni LLC.
+//  Copyright 2021 DigiSomni LLC.
 //
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
